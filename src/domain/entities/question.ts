0af84ce310f39fbb881f3f012856148990219ee9
Kow <kaionesyan@gmail.com>
2023-04-28 14:42:46 -0300

    chore: initial commit

---
+++ src/domain/entities/question.ts
@@ -0,0 +1,89 @@
+import { Slug } from "./value-objects/slug";
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
+import dayjs from "dayjs";
+
+interface QuestionProps {
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
+  title: string;
+  content: string;
+  slug: Slug;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
+export class Question extends Entity<QuestionProps> {
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  get slug() {
+    return this.props.slug;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, "days") <= 3;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat("...");
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set bestAnswerId(value: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = value;
+    this.touch();
+  }
+
+  set title(value: string) {
+    this.props.title = value;
+    this.props.slug = Slug.createFromText(value);
+    this.touch();
+  }
+
+  set content(value: string) {
+    this.props.content = value;
+    this.touch();
+  }
+
+  static create(
+    props: Optional<QuestionProps, "createdAt" | "slug">,
+    id?: UniqueEntityID
+  ) {
+    const question = new Question(
+      {
+        ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
+        createdAt: new Date(),
+      },
+      id
+    );
+
+    return question;
+  }
+}
---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,20 @@
+import { AnswerQuestionUseCase } from "./answer-question";
+import { AnswersRepository } from "../repositories/answers-repository";
+
+const fakeAnswersRepository: AnswersRepository = {
+  create: async function () {
+    return;
+  },
+};
+
+test("create an answer", async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository);
+
+  const answer = await answerQuestion.execute({
+    questionId: "question-id",
+    instructorId: "instructor-id",
+    content: "New answer",
+  });
+
+  expect(answer.content).toEqual("New answer");
+});
